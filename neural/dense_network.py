fd99110d9196629f6deb97d1fdd852a3126164f3
Vladislav Antonov <antonovdev95@gmail.com>
2023-06-28 05:25:58 +0300

    Refactor DenseNetwork: configurable arch, shared _forward, get_info, fit history

--- neural/dense_network.py
+++ neural/dense_network.py
@@ -5,13 +5,20 @@ import numpy as np
 import sys
 import time
 from pathlib import Path
+from typing import Callable
+
+try:
+    import h5py
+except ModuleNotFoundError:
+    h5py = None
 
 _MIN_SAMPLES_PER_WORKER = 5_000
 
+
 class DenseNetwork:
-    def __init__(self):
+    def __init__(self, hidden_size: int = 100):
         self.input_size = 784
-        self.hidden_size = 100
+        self.hidden_size = hidden_size
         self.output_size = 10
 
         self.weights_0_1 = None
@@ -23,7 +30,39 @@ class DenseNetwork:
         self.weights_1_2 = 0.2 * np.random.random((self.hidden_size, self.hidden_size)) - 0.1
         self.weights_2_3 = 0.2 * np.random.random((self.hidden_size, self.output_size)) - 0.1
 
-    def fit(self, x_train, y_train, batch_size, epochs, validation_split=0.1, alpha=0.01, num_workers=None):
+    def _forward(self, x: np.ndarray) -> np.ndarray:
+        l1 = tanh(x @ self.weights_0_1)
+        l2 = tanh(l1 @ self.weights_1_2)
+        return softmax(l2 @ self.weights_2_3)
+
+    def get_info(self) -> dict:
+        initialized = all(w is not None for w in [self.weights_0_1, self.weights_1_2, self.weights_2_3])
+        info = {
+            'input_size': self.input_size,
+            'hidden_size': self.hidden_size,
+            'output_size': self.output_size,
+            'initialized': initialized,
+        }
+        if initialized:
+            info['shapes'] = {
+                'weights_0_1': self.weights_0_1.shape,
+                'weights_1_2': self.weights_1_2.shape,
+                'weights_2_3': self.weights_2_3.shape,
+            }
+            info['total_params'] = self.weights_0_1.size + self.weights_1_2.size + self.weights_2_3.size
+        return info
+
+    def fit(
+        self,
+        x_train,
+        y_train,
+        batch_size: int,
+        epochs: int,
+        validation_split: float = 0.1,
+        alpha: float = 0.01,
+        num_workers: int | None = None,
+        on_epoch_end: Callable[[int, float, float], None] | None = None,
+    ) -> dict:
         if self.weights_0_1 is None or self.weights_1_2 is None or self.weights_2_3 is None:
             self.init_weights()
 
@@ -42,7 +81,7 @@ class DenseNetwork:
             raise ValueError("x_train and y_train must have the same length")
 
         validation_size = int(len(x_train) * validation_split)
-        train_size = int(len(x_train) - validation_size)
+        train_size = len(x_train) - validation_size
 
         if num_workers is None:
             num_workers = min(
@@ -52,19 +91,14 @@ class DenseNetwork:
         else:
             num_workers = max(1, min(num_workers, multiprocessing.cpu_count()))
 
+        history: dict[str, list[float]] = {'train_acc': [], 'val_acc': []}
+
         for e in range(epochs):
-            correct_cnt = 0
-            test_correct_cnt = 0
-
             perm = np.random.permutation(len(x_train))
-            x_shuf = x_train[perm]
-            y_shuf = y_train[perm]
-
-            train_images = x_shuf[:train_size]
-            train_labels = y_shuf[:train_size]
-
-            validation_images = x_shuf[train_size:]
-            validation_labels = y_shuf[train_size:]
+            train_images = x_train[perm[:train_size]]
+            train_labels = y_train[perm[:train_size]]
+            val_images   = x_train[perm[train_size:]]
+            val_labels   = y_train[perm[train_size:]]
 
             start = time.monotonic()
 
@@ -91,27 +125,27 @@ class DenseNetwork:
             self.weights_1_2 += alpha * dw12 / total_batches
             self.weights_2_3 += alpha * dw23 / total_batches
 
+            train_acc = correct_cnt / float(train_size)
+
+            val_acc = 0.0
             if validation_size > 0:
-                num_val_batches = int(np.ceil(validation_size / float(batch_size)))
-                for i in range(num_val_batches):
-                    batch_start, batch_end = (i * batch_size), min((i + 1) * batch_size, validation_size)
-                    layer_0 = validation_images[batch_start:batch_end]
-                    layer_1 = tanh(np.dot(layer_0, self.weights_0_1))
-                    layer_2 = tanh(np.dot(layer_1, self.weights_1_2))
-                    layer_3 = softmax(np.dot(layer_2, self.weights_2_3))
+                val_probs = self._forward(val_images)
+                val_acc = float((val_probs.argmax(axis=1) == val_labels.argmax(axis=1)).mean())
 
-                    test_correct_cnt += int(
-                        (layer_3.argmax(axis=1) == validation_labels[batch_start:batch_end].argmax(axis=1)).sum()
-                    )
+            history['train_acc'].append(train_acc)
+            history['val_acc'].append(val_acc)
 
-            end = time.monotonic() - start
-
+            elapsed = time.monotonic() - start
             sys.stdout.write(
-                f"I:{e} || Test-Acc:{test_correct_cnt / float(max(1, validation_size))} "
-                f"|| Train-Acc:{correct_cnt / float(train_size)} "
-                f"|| {end}s\n")
+                f"I:{e} || Val-Acc:{val_acc:.4f} || Train-Acc:{train_acc:.4f} || {elapsed:.2f}s\n"
+            )
 
-    def predict(self, x):
+            if on_epoch_end is not None:
+                on_epoch_end(e, train_acc, val_acc)
+
+        return history
+
+    def predict(self, x) -> np.ndarray:
         x = np.asarray(x)
         if x.ndim == 1:
             x = x.reshape(1, -1)
@@ -119,18 +153,12 @@ class DenseNetwork:
             raise ValueError(f"x must have shape (N, {self.input_size}) or ({self.input_size},), got {x.shape}")
         if self.weights_0_1 is None or self.weights_1_2 is None or self.weights_2_3 is None:
             raise RuntimeError("Model is not initialized. Call fit() or init_weights() first.")
+        return self._forward(x).argmax(axis=1)
 
-        layer_1 = tanh(np.dot(x, self.weights_0_1))
-        layer_2 = tanh(np.dot(layer_1, self.weights_1_2))
-        probs = softmax(np.dot(layer_2, self.weights_2_3))
-        return probs.argmax(axis=1)
-
-    def evaluate(self, x_test, y_test, batch_size: int = 1024, verbose: bool = True) -> float:
+    def evaluate(self, x_test, y_test, verbose: bool = True) -> float:
         x_test = np.asarray(x_test)
         y_test = np.asarray(y_test)
 
-        if batch_size <= 0:
-            raise ValueError("batch_size must be > 0")
         if x_test.ndim != 2 or x_test.shape[1] != self.input_size:
             raise ValueError(f"x_test must have shape (N, {self.input_size}), got {x_test.shape}")
         if y_test.ndim != 2 or y_test.shape[1] != self.output_size:
@@ -140,33 +168,17 @@ class DenseNetwork:
         if self.weights_0_1 is None or self.weights_1_2 is None or self.weights_2_3 is None:
             raise RuntimeError("Model is not initialized. Call fit() or init_weights() first.")
 
-        correct_cnt = 0
-        n = int(x_test.shape[0])
-        num_batches = int(np.ceil(n / float(batch_size)))
-        for i in range(num_batches):
-            batch_start, batch_end = (i * batch_size), min((i + 1) * batch_size, n)
-            xb = x_test[batch_start:batch_end]
-            yb = y_test[batch_start:batch_end]
-
-            layer_1 = tanh(np.dot(xb, self.weights_0_1))
-            layer_2 = tanh(np.dot(layer_1, self.weights_1_2))
-            probs = softmax(np.dot(layer_2, self.weights_2_3))
-
-            correct_cnt += int((probs.argmax(axis=1) == yb.argmax(axis=1)).sum())
-
-        acc = correct_cnt / float(max(1, n))
+        probs = self._forward(x_test)
+        acc = float((probs.argmax(axis=1) == y_test.argmax(axis=1)).mean())
         if verbose:
-            sys.stdout.write(f"Test-Acc:{acc}\n")
+            sys.stdout.write(f"Test-Acc:{acc:.4f}\n")
         return acc
 
     def save_model(self, path: str | Path) -> None:
         if self.weights_0_1 is None or self.weights_1_2 is None or self.weights_2_3 is None:
             raise RuntimeError("Model is not initialized. Call fit() or init_weights() first.")
-
-        try:
-            import h5py  # type: ignore
-        except ModuleNotFoundError as e:
-            raise ModuleNotFoundError("h5py is required for HDF5 save/load. Install it via requirements.txt.") from e
+        if h5py is None:
+            raise ModuleNotFoundError("h5py is required for HDF5 save/load. Install it via requirements.txt.")
 
         path = Path(path)
         path.parent.mkdir(parents=True, exist_ok=True)
@@ -183,46 +195,37 @@ class DenseNetwork:
             w.create_dataset("weights_2_3", data=self.weights_2_3)
 
     def load_model(self, path: str | Path) -> None:
-        try:
-            import h5py  # type: ignore
-        except ModuleNotFoundError as e:
-            raise ModuleNotFoundError("h5py is required for HDF5 save/load. Install it via requirements.txt.") from e
+        if h5py is None:
+            raise ModuleNotFoundError("h5py is required for HDF5 save/load. Install it via requirements.txt.")
 
         path = Path(path)
         with h5py.File(path, "r") as f:
             arch = f["arch"].attrs
-            input_size = int(arch["input_size"])
-            hidden_size = int(arch["hidden_size"])
-            output_size = int(arch["output_size"])
-
-            if (input_size, hidden_size, output_size) != (self.input_size, self.hidden_size, self.output_size):
-                raise ValueError(
-                    "Model architecture mismatch: "
-                    f"file has ({input_size}, {hidden_size}, {output_size}), "
-                    f"but instance is ({self.input_size}, {self.hidden_size}, {self.output_size})"
-                )
+            self.input_size  = int(arch["input_size"])
+            self.hidden_size = int(arch["hidden_size"])
+            self.output_size = int(arch["output_size"])
 
             w = f["weights"]
             self.weights_0_1 = np.asarray(w["weights_0_1"], dtype=np.float64)
             self.weights_1_2 = np.asarray(w["weights_1_2"], dtype=np.float64)
             self.weights_2_3 = np.asarray(w["weights_2_3"], dtype=np.float64)
 
-def tanh(x):
+
+def tanh(x: np.ndarray) -> np.ndarray:
     return np.tanh(x)
 
 
-def tanh2deriv(x):
+def tanh2deriv(x: np.ndarray) -> np.ndarray:
     return 1 - (x ** 2)
 
 
-def softmax(x):
+def softmax(x: np.ndarray) -> np.ndarray:
     x = x - np.max(x, axis=1, keepdims=True)
-    temp = np.exp(x)
-    return temp / np.sum(temp, axis=1, keepdims=True)
+    e = np.exp(x)
+    return e / np.sum(e, axis=1, keepdims=True)
 
 
 def _train_shard(args):
-    """Compute accumulated gradient updates for one data shard (runs in a worker process)."""
     x_shard, y_shard, w01, w12, w23, batch_size = args
     shard_size = x_shard.shape[0]
 
@@ -238,23 +241,22 @@ def _train_shard(args):
         bs = batch_end - batch_start
 
         layer_0 = x_shard[batch_start:batch_end]
-        layer_1 = tanh(np.dot(layer_0, w01))
+        layer_1 = tanh(layer_0 @ w01)
         dropout_mask = np.random.randint(2, size=layer_1.shape)
         layer_1_dropout = layer_1 * dropout_mask * 2
-        layer_2 = tanh(np.dot(layer_1_dropout, w12))
-        layer_3 = softmax(np.dot(layer_2, w23))
+        layer_2 = tanh(layer_1_dropout @ w12)
+        layer_3 = softmax(layer_2 @ w23)
 
         correct_cnt += int(
             (layer_3.argmax(axis=1) == y_shard[batch_start:batch_end].argmax(axis=1)).sum()
         )
 
         layer_3_delta = (y_shard[batch_start:batch_end] - layer_3) / float(bs)
-        layer_2_delta = layer_3_delta.dot(w23.T) * tanh2deriv(layer_2)
-        layer_1_delta = layer_2_delta.dot(w12.T) * tanh2deriv(layer_1)
-        layer_1_delta *= dropout_mask * 2
+        layer_2_delta = layer_3_delta @ w23.T * tanh2deriv(layer_2)
+        layer_1_delta = layer_2_delta @ w12.T * tanh2deriv(layer_1) * dropout_mask * 2
 
-        dw23 += layer_2.T.dot(layer_3_delta)
-        dw12 += layer_1_dropout.T.dot(layer_2_delta)
-        dw01 += layer_0.T.dot(layer_1_delta)
+        dw23 += layer_2.T @ layer_3_delta
+        dw12 += layer_1_dropout.T @ layer_2_delta
+        dw01 += layer_0.T @ layer_1_delta
 
     return dw01, dw12, dw23, correct_cnt
